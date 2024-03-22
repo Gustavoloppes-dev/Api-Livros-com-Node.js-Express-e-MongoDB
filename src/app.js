64789796bd51618c2bd495ab15c411c0adb6874b
Gustavoloppes-dev <gustavoloppes.dev@gmail.com>
2024-03-22 15:55:45 -0300

    Relacionando entidades de livro e autor, e criação da query de buscar livro por editora

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livros.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -13,49 +13,14 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json())
+routes(app);
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
 
 function buscaLivro(id) {
     return livros.findIndex(livro => {
         return livro.id === Number(id)
     })
 }
-
-app.get("/", (req,res) => {
-    res.status(200).send("Curso Node.js");
-})
-
-app.get("/livros", async (req,res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros)
-})
-
-app.get("/livros/:id", (req,res) => {
-    const index = buscaLivro(req.params.id)
-    res.status(200).json(livros[index]);
-})
-
-app.post("/livros", (req,res) => {
-    livros.push(req.body)
-    res.status(201).send("Livro registrado com sucesso!")
-})
-
-app.put("/livros/:id", (req,res) => {
-    const index = buscaLivro(req.params.id)
-    livros[index].titulo = req.body.titulo
-    res.status(200).json(livros)
-})
 
 app.delete("/livros/:id", (req,res) => {
     const index = buscaLivro(req.params.id);
